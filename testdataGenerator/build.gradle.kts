7355d499c682bc9343402cbbd306f7e822a50df7
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2022-04-14 12:12:14 +0200

    Implemented task to create standalone testdata generator app

--- testdataGenerator/build.gradle.kts
+++ testdataGenerator/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.panteleyev.jpackage.JPackageTask
+
 plugins {
     kotlin("jvm")
     id("com.github.johnrengelman.shadow")
     id("org.openjfx.javafxplugin")
+    id("org.panteleyev.jpackageplugin")
 }
 
 kotlin {
@@ -24,6 +27,31 @@ tasks {
         }
         archiveFileName.set("MDTestdataGenerator.jar")
     }
+}
+
+//FOLLOWING TASKS CREATE SYSTEM DEPENDENT BINARY WITH JRE
+task("copyDependencies", Copy::class) {
+    from(configurations.runtimeClasspath).into("$buildDir/jars")
+}
+
+task("copyJar", Copy::class) {
+    from(tasks.jar).into("$buildDir/jars")
+}
+
+tasks.register<JPackageTask>("CreateAppImage") {
+    dependsOn("build", "copyDependencies", "copyJar")
+
+    input = "$buildDir/jars"
+    destination = "$buildDir/dist"
+
+    appName = "MREReport-Testdata-Generator"
+    vendor = "Institut für Medizinische Informatik Münster"
+
+    mainJar = tasks.jar.get().archiveFileName.get()
+    mainClass = "de.uni_muenster.imi.oegd.testdataGenerator.Main"
+
+    javaOptions = listOf("-Dfile.encoding=UTF-8")
+    type = org.panteleyev.jpackage.ImageType.APP_IMAGE
 }
 
 javafx {
