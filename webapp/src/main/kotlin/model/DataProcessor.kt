8d4a4d4a4fa0a2c5e368a5bf182938af3c355b86
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2023-01-13 09:36:54 +0100

    Adjusted DataProcessor to search for the language code instead of the german translation

--- webapp/src/main/kotlin/model/DataProcessor.kt
+++ webapp/src/main/kotlin/model/DataProcessor.kt
@@ -3,27 +3,27 @@ package model
 object DataProcessor {
     fun countMRSATotal(caseList: List<Map<String, String>>) = caseList.size
 
-    fun countMRSANosokomial(caseList: List<Map<String, String>>) = caseList.count { it["nosokomial?"] == "nosokomial" }
+    fun countMRSANosokomial(caseList: List<Map<String, String>>) = caseList.count { it["page.MRSA.caselist.nosocomial"] == "nosokomial" }
 
-    fun countMRSAImported(caseList: List<Map<String, String>>) = caseList.count { it["nosokomial?"] != "nosokomial" }
+    fun countMRSAImported(caseList: List<Map<String, String>>) = caseList.count { it["page.MRSA.caselist.nosocomial"] != "nosokomial" }
 
-    fun countMRGN3Cases(caseList: List<Map<String, String>>) = caseList.count { it["Klasse"] == "MRGN3" }
+    fun countMRGN3Cases(caseList: List<Map<String, String>>) = caseList.count { it["page.MRGN.caselist.class"] == "MRGN3" }
 
-    fun countMRGN4Cases(caseList: List<Map<String, String>>) = caseList.count { it["Klasse"] == "MRGN4" }
+    fun countMRGN4Cases(caseList: List<Map<String, String>>) = caseList.count { it["page.MRGN.caselist.class"] == "MRGN4" }
 
     fun countVREEfaeciumResistant(caseList: List<Map<String, String>>) = caseList.count {
-        it["Vancomycin Ergebnis"] == "R" && it["Erreger"] == "Enterococcus faecium"
+        it["page.VRE.caselist.vancomycin"] == "R" && it["page.VRE.caselist.pathogen"] == "Enterococcus faecium"
     }
 
     fun countVREEfaeciumTotal(caseList: List<Map<String, String>>) = caseList.count {
-        (it["Vancomycin Ergebnis"] == "R" || it["Vancomycin Ergebnis"] == "S") && it["Erreger"] == "Enterococcus faecium"
+        (it["page.VRE.caselist.vancomycin"] == "R" || it["page.VRE.caselist.vancomycin"] == "S") && it["page.VRE.caselist.pathogen"] == "Enterococcus faecium"
     }
 
     fun countVREEfaecalisResistant(caseList: List<Map<String, String>>) = caseList.count {
-        it["Vancomycin Ergebnis"] == "R" && it["Erreger"] == "Enterococcus faecalis"
+        it["page.VRE.caselist.vancomycin"] == "R" && it["page.VRE.caselist.pathogen"] == "Enterococcus faecalis"
     }
 
     fun countOtherCases(caseList: List<Map<String, String>>) = caseList.count {
-        it["Erreger"] != "Enterococcus faecalis" && it["Erreger"] != "Enterococcus faecium"
+        it["page.VRE.caselist.pathogen"] != "Enterococcus faecalis" && it["page.VRE.caselist.pathogen"] != "Enterococcus faecium"
     }
 }
