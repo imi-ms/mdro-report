d7d0bf52a4eabdc65b10982f9ff93d94136650d3
Johannes Oehm <johannes.oehm@uni-muenster.de>
2025-03-21 08:40:02 +0100

    updating kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.panteleyev.jpackage.JPackageTask
 System.setProperty("user.dir", project.projectDir.toString())
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
     java
     application
     id("org.openjfx.javafxplugin") version "0.1.0"
